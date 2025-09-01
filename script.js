affd4ca2e820eecdd39bbbaba3d441029d0c6678
DevojuSharathChandra <sharathdevoju1@gmail.com>
2025-09-01 22:09:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function downloadResume() {
-    // Replace with your actual resume file URL
-    const link = document.createElement('a');
-    link.href = 'Sharath_Devoju_Resume.pdf'; // Place this file in the same folder or change the path accordingly
-    link.download = 'Sharath_Devoju_Resume.pdf';
-    link.click();
-  }
-  
